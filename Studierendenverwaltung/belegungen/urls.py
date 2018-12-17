52fa47b4213cf5de5babaaa2c6524cfbe671c50b
Tzoulia Malousari <41481397+2mee@users.noreply.github.com>
2018-12-17 03:44:13 +0100

    Update urls.py

--- Studierendenverwaltung/belegungen/urls.py
+++ Studierendenverwaltung/belegungen/urls.py
@@ -14,11 +14,10 @@ urlpatterns = [                  # urlpatterns += [] auch möglich
     # 127.0.0.1:8000/belegungen/2/
     path('<int:lv_id>/', views.modul_detail, name='modul_detail'),
     path('<int:lv_id>/', views.modulwahl, name='modulwahl'),
-    path('student_detail/', views.student_detail, name='student_detail'),
+    # path('student_detail/', views.student_detail, name='student_detail'),
     # 127.0.0.1:8000/belegungen/results
     path('results/', views.results, name='results'),
     # 127.0.0.1:8000/belegungen/belegungsanzahl
     path('belegungsanzahl/', views.belegungsanzahl, name='belegungsanzahl'),
 
 ]
-
